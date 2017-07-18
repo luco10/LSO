723ef4f5f5ae5dc18907fd5cf1c751b0aeacecd0
luco10 <senku10@hotmail.it>
2017-07-18 18:16:32 +0200

    Add files via upload

--- Client/main.c
+++ Client/main.c
@@ -1,43 +1,43 @@
-
 #include<stdio.h> //printf
 #include<string.h>    //strlen
 #include<sys/socket.h>    //socket
 #include<arpa/inet.h> //inet_addr
 
-int main(int argc , char **argv)
-{
-    int sock;
+int main(int argc , char **argv){
+    int server_sock;
     struct sockaddr_in server;
     char message[1000] , server_reply[2000];
 
-    /* Controlla il numero dei parametri da linea di comando */
-	if( argc < 6)
-	{
-		write( 1, "\nParametri non inseriti da linea di comando\n", strlen("\nParametri non inseriti da linea di comando\n"));
+    // Controlla il numero dei parametri da linea di comando
+	if(argc < 6){
+		write(1, "Parametri non inseriti da linea di comando\n", strlen("Parametri non inseriti da linea di comando\n"));
 		exit(EXIT_FAILURE);
 	}
 
-    //Create socket
-    sock = socket(AF_INET , SOCK_STREAM , 0);
-    if (sock == -1)
-    {
-        printf("Could not create socket");
+    // Creazione socket per connessione al Server
+    if((server_sock = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+        perror("\nErrore socket");
+        exit(EXIT_FAILURE);
     }
-    puts("Socket created");
 
-    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    // Inizializzazione della struttura sockaddr_in
+    server.sin_addr.s_addr = inet_addr(argv[1]);
     server.sin_family = AF_INET;
     server.sin_port = htons(atoi(argv[2]));
 
-    //Connect to remote server
-    if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
-    {
-        perror("connect failed. Error");
-        return 1;
+    // Connessione al Server
+    if (connect(server_sock, (struct sockaddr *)&server, sizeof(server)) < 0){
+        perror("\nErrore connessione");
+        exit(EXIT_FAILURE);
     }
 
-    puts("Connected\n");
+    // Invia i parametri (IP e porta) al Server
+    write(server_sock, parametri, sizeof(int)*3);
 
+    /* Va modifica la write sopra, inoltre sotto va gestita la risposta del server(Vedi come ha fatto peolo in Client.c) */
+
+
+    /*
     //keep communicating with server
     while(1)
     {
@@ -61,7 +61,8 @@ int main(int argc , char **argv)
         puts("Server reply :");
         puts(server_reply);
     }
+    */
 
-    close(sock);
+    close(server_sock);
     return 0;
 }
